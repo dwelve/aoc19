a27585939cb2aca2fa81b6bd125dff8296f32813
Lue Andrie-Her <lue.her@gmail.com>
2019-12-08 21:16:13 -0600

    Added Day6 Part 2 solution

--- src/main/kotlin/dev/lue/aoc19/Day6.kt
+++ src/main/kotlin/dev/lue/aoc19/Day6.kt
@@ -1,23 +1,13 @@
 package dev.lue.aoc19
 
+import java.util.*
+import kotlin.collections.HashMap
+
 class Day6 : IDay {
     override val part1InputFilename: String = "6.txt"
     override val part2InputFilename: String = "6.txt"
 
-    fun parseInput(raw_input: String): Map<String, Set<String>> {
-        /*
-COM)B
-B)C
-C)D
-D)E
-E)F
-B)G
-G)H
-D)I
-E)J
-J)K
-K)L
-         */
+    fun parseInput(raw_input: String, undirected: Boolean = false): Map<String, Set<String>> {
         val map : HashMap<String, MutableSet<String>> = hashMapOf()
         for (row in raw_input.trim().split("\n")) {
             val edge = row.split(")")
@@ -28,13 +18,20 @@ K)L
             } else{
                 map[src]!!.add(dst)
             }
+            if (undirected) {
+                if (!map.containsKey(dst)) {
+                    map[dst] = mutableSetOf(src)
+                } else{
+                    map[dst]!!.add(src)
+                }
+            }
         }
         return map
     }
 
     override fun runPart1(raw_input: String): Int {
         val graph = parseInput(raw_input)
-        println("$graph")
+        //println("$graph")
         return dfs("COM", 0, graph)
     }
 
@@ -46,7 +43,33 @@ K)L
         return b + a
     }
 
+    data class Node(val depth: Int, val vertex: String)
+
     override fun runPart2(raw_input: String): Int {
+        val graph = parseInput(raw_input, undirected = true)
+        //println("$graph")
+        // do breadth first search
+        val start = "YOU"
+        val goal = "SAN"
+        val q: Deque<Node> = LinkedList()
+        val visited = mutableSetOf<String>()
+
+        q.addLast(Node(0, start))
+
+        while (q.isNotEmpty()) {
+            val node = q.pollFirst()
+            if (node.vertex == goal) {
+                return node.depth - 2
+            }
+            for (nei in graph.getOrDefault(node.vertex, emptySet())) {
+                if (!visited.contains(nei)) {
+                    visited.add(nei)
+                    val next_node = Node(node.depth+1, nei)
+                    q.addLast(next_node)
+                }
+            }
+        }
+        println("No path found :(")
         return -1
     }
 }
